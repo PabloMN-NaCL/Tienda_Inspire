6993dae8fd05e956186c25c8d67df250118e659d
PabloMNaCl <millannavarropablo@gmail.com>
2025-12-05 18:58:18 +0100

    Añadir eventos de createdOrder y canceled order

---
+++ TiendaInspire.Shared/Events/OrderCanceledEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TiendaInspire.Shared.Events
+{
+    public sealed record OrderCancelledEvent(
+      int OrderId,
+      string UserId,
+      IEnumerable<OrderItemEvent> Items) : IRabbitEvent
+    {
+        public Guid EventId { get; init; } = Guid.NewGuid();
+        public DateTime CreateAt { get; init; } = DateTime.UtcNow;
+    }
---
+++ TiendaInspire.Shared/Events/OrderCreatedEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TiendaInspire.Shared.Events
+{
+
+
+    public sealed record OrderCreatedEvent(
+        int OrderId,
+        string UserId,
+        IEnumerable<OrderItemEvent> Items) : IRabbitEvent
+    {
+        public Guid EventId { get; init; } = Guid.NewGuid();
+        public DateTime CreateAt { get; init; } = DateTime.UtcNow;
+    }
+
+    public sealed record OrderItemEvent(
+        int ProductId,
+        string ProductName,
+        int Quantity);
+}
